766b126c9b92102ae0f6c043fff0f29a1e4ec5e4
Jack Gray <57096830+Jackgray26@users.noreply.github.com>
2020-11-04 17:01:37 +0000

    Add Text and a rectangle
    Code for text on the red circle, and for a rectangle in top left

--- PythonPracticeGraphics1.py
+++ PythonPracticeGraphics1.py
@@ -9,3 +9,11 @@ center = Point(100, 100)
 circ = Circle(center, 30)
 circ.setFill('red')
 circ.draw(win)
+
+# Addition of text to circle
+label = Text(center, "Red Circle")
+label.draw(win)
+
+# Add a clear rectangle to image
+rect = Rectangle(Point(20, 30), Point(70, 70))
+rect.draw(win)
