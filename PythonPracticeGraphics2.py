b6712750670e4c02903bae7d611c791cecd84316
Jack Gray <57096830+Jackgray26@users.noreply.github.com>
2020-11-11 16:41:35 +0000

    Window Sizing and Title
    Added a Window of 320 x 240 px and a title

--- PythonPracticeGraphics2.py
+++ PythonPracticeGraphics2.py
@@ -1,3 +1,7 @@
 # Code to calculate the growth of an investment over 10 years, with a graphical output
 
 
+# Create window with 320x240 resolution for readability and usability
+# Add the title of Investment Growth Chartt
+
+win = GraphWin("Investment Growth Chart", 320, 240)
