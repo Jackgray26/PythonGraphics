69d83a70ae98c041978301fbeb0d4e2892e1b43c
Jack Gray <57096830+Jackgray26@users.noreply.github.com>
2020-11-11 16:47:29 +0000

    Y Axis Labels
    added Y axis labels from 0 to 10K

--- PythonPracticeGraphics2.py
+++ PythonPracticeGraphics2.py
@@ -1,7 +1,19 @@
 # Code to calculate the growth of an investment over 10 years, with a graphical output
 
+from graphics import * 
+
 
 # Create window with 320x240 resolution for readability and usability
 # Add the title of Investment Growth Chartt
 
 win = GraphWin("Investment Growth Chart", 320, 240)
+
+# Y Axis labels up to 10 Thousand
+# starting at 10 px up from bottom and 20 in
+
+Draw label " 0.0K" at (20, 230)
+Draw Label " 2.5K" at (20, 180)
+Draw Label " 5.0K" at (20, 130)
+Draw Label " 275K" at (20, 80)
+Draw Label " 10.0K" at (20, 30)
+
