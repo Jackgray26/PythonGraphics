a159243b7284ca7b7414fd3264e24016bc2b4f21
Jack Gray <57096830+Jackgray26@users.noreply.github.com>
2020-11-12 15:59:14 +0000

    for loop
    tracks where the clicks occur based on .getMouse

--- PythonPracticeGraphics3.py
+++ PythonPracticeGraphics3.py
@@ -8,3 +8,10 @@ from graphics import *
 # Create a clickable box
 def main():
     win = GraphWin("Click Here!")
+    # For loop that will report where a click occurs
+    for i in range(10):
+        p = win.getMouse()
+        print("Click at: ", p.getX(), p.getX())
+
+main()
+
