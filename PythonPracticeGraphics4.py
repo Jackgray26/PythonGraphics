db7d26415290e24d087d53611633d52f3516caeb
Jack Gray <57096830+Jackgray26@users.noreply.github.com>
2020-11-12 16:53:56 +0000

    Update PythonPracticeGraphics4.py

--- PythonPracticeGraphics4.py
+++ PythonPracticeGraphics4.py
@@ -5,4 +5,10 @@
 from graphics import *
 
 def main():
+
+    win = GraphWin("Click to place the points of a triangle.")
+    win.setCoords(0.0, 0.0, 10.0, 10.0)
+    message = Text(Point(5, 0.5), "Click in three Locations")
+    message.draw(win)
+
     
