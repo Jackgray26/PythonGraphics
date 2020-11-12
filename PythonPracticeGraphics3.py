67a0572f8b2a7ceb653e357ec64e7b222a427896
Jack Gray <57096830+Jackgray26@users.noreply.github.com>
2020-11-12 15:56:43 +0000

    Import and create main

--- PythonPracticeGraphics3.py
+++ PythonPracticeGraphics3.py
@@ -1,1 +1,10 @@
-{\rtf1}
+# PythonPracticeGraphics.py
+# To try interatcive graphics with Python
+
+# Import graphics.py
+
+from graphics import *
+
+# Create a clickable box
+def main():
+    win = GraphWin("Click Here!")
