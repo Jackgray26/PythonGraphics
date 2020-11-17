544597e88cecf529bc7dec1082bf87bd341af4a2
Jack Gray <57096830+Jackgray26@users.noreply.github.com>
2020-11-17 16:54:06 +0000

    For Loop
    To handle user input

--- PythonPracticeGraphics5.py
+++ PythonPracticeGraphics5.py
@@ -3,4 +3,9 @@
 from graphics import #
 
 def main():
-    
+    win =  GraphWin("Click to place Text", 400, 400)
+    # for loop to take user input, and start drawing text
+    for i in range(10):
+        pt = win.getMouse()
+        key = win.getKey()
+        
